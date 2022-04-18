217800b174ec0fd0fa9739da6b645479297e5f9e
elvis <elvis.com.web.app@gmail.com>
2022-04-18 00:04:07 -0300

    Primera Subida

---
+++ app/src/main/java/elvis/com/frament/MainActivity.java
@@ -0,0 +1,21 @@
+package elvis.com.frament;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import elvis.com.frament.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
